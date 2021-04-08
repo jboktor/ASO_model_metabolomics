c733e08c76db2d4d1e6131d1f78981f844715c26
Joseph Boktor <josephboktor@Josephs-MacBook-Pro.local>
2021-04-07 18:13:56 -0700

    clean functions

--- src/functions.R
+++ src/functions.R
@@ -4,142 +4,7 @@
 #------------------------------------------------------------------------------
 #  Functions
 #------------------------------------------------------------------------------
-GO_bubble_plot <- function(df){
-  
-  df %>% 
-    top_n(15, wt=-q.value.FDR.B.H) %>% 
-    mutate(hitsPerc=Hit.Count.in.Query.List*100/Hit.Count.in.Genome) %>%
-    arrange(hitsPerc) %>%
-    mutate(Name=factor(Name, levels=Name)) %>%
-    ggplot(aes(x=hitsPerc, 
-               y=Name, 
-               colour=q.value.FDR.B.H, 
-               size=Hit.Count.in.Query.List)) +
-    geom_point() +
-    theme_bw()+
-    scale_color_viridis_c(option = "cividis") +
-    expand_limits(x=1) +
-    labs(x="Hits (%)", y="", colour="FDR", size="Count")
-}
 
-#------------------------------------------------------------------------------
-
-GO_barplot <- function(df, fill){
-  
-  df %>% 
-    drop_na("q.value.FDR.B.H") %>%
-    mutate(Hit.Count.in.Query.List = as.integer(Hit.Count.in.Query.List)) %>%
-    dplyr::mutate(hitsPerc=Hit.Count.in.Query.List*100/Hit.Count.in.Genome) %>%
-    dplyr::arrange(q.value.FDR.B.H, desc(hitsPerc)) %>%
-    slice_head(n = 15) %>% 
-    dplyr::mutate(yvar = paste(Name, ID)) %>% 
-    ggplot(aes(x= -log10(q.value.FDR.B.H), 
-               y=reorder(yvar, -q.value.FDR.B.H))) +
-    geom_bar(stat = "identity", fill = fill, width = 0.5) + 
-    labs(x = expression(paste(log[10], " (Adjusted P-value)")), 
-         y = "") +
-    theme_classic() +
-    scale_x_continuous(expand = c(0, 0)) 
-}
-
-#------------------------------------------------------------------------------
-
-
-xysummary <- function(df, genekeydf, siglist,
-                      x, y, xgroup, ygroup, title, fill, force){
-  
-  cols <- c("nodiff" = "#808080", "diff" = fill)
-  col.rims <- c("nodiff" = "#ffffff", "diff" = fill)
-  
-  # remerge genename
-  # select df of only significant genes
-  sigfilter <- siglist %>% 
-    dplyr::arrange(q_value, desc(abs(log2.fold_change.))) %>%
-    slice_head(n = 25)
-  
-  cat("Top 20 most significant genes: \n\n")
-  print(sigfilter$gene)
-  
-  df %>% 
-    dplyr::mutate(genelabels = if_else(gene %in% sigfilter$gene, gene, "")) %>% 
-    dplyr::mutate(goi_col = if_else(gene %in% siglist$gene, "diff", "nodiff")) %>% 
-    dplyr::mutate(log2_value_1 = log2(value_1 + 1)) %>% 
-    dplyr::mutate(log2_value_2 = log2(value_2 + 1)) %>%
-    dplyr::arrange(desc(goi_col)) %>% 
-    ggplot(aes(x=log2_value_1, y=log2_value_2)) +
-    geom_point(aes(fill = goi_col, color = goi_col),
-               shape=21, size=0.7, alpha = 0.7) +
-    geom_text_repel(aes(label = genelabels), segment.alpha = 0.2, segment.size = 0.2, size =1.75, force = force, max.iter=6000) +
-    theme_classic() +
-    geom_abline(intercept = 0, slope = 1) +
-    labs(x = paste0("expression (log2 FPKM + 1): ", xgroup),
-         y = paste0("expression (log2 FPKM + 1): ", ygroup),
-         title = title) +
-    scale_color_manual(values = col.rims, name ="Group") +
-    scale_fill_manual(values = cols, name ="Group") +
-    theme(legend.position = "none", 
-          plot.title = element_text(hjust = 0.5))
-}
-
-
-
-#------------------------------------------------------------------------------
-boxplot_transformed <- function(goi, cols = group.cols, 
-                    title = blank.title, ylabel = blank.ylabel){
-  
-  blank.title = " "; blank.ylabel = " "
-  group.cols = c("ASO_GF"= "#FF6000", "ASO_SPF" = "#077E97", 
-                 "WT_GF" = "#A00000", "WT_SPF" = "808080")
-  
-  set.seed(123)
-  
-  df.long %>% 
-    filter(Genes == goi) %>% 
-    ggplot(aes(x=group, y= log2(count + 1))) +
-    geom_boxplot(aes(fill = group), alpha = 0.75, outlier.alpha = 0, width = 0.9) +
-    geom_point(aes(fill = group), position = position_jitterdodge(jitter.width = 0.5), 
-               shape=21, size=1.5, alpha = 1) +
-    theme_classic() +
-    ggtitle(goi) +
-    labs(y = expression(paste(log[2], " (FPKM + 1)"))) +
-    scale_color_manual(values = cols, name ="Group") +
-    scale_fill_manual(values = cols, name ="Group") +
-    theme(plot.title = element_text(hjust = 0.5),
-          axis.title.x = element_blank(),
-          panel.grid.major.y = element_blank(),
-          legend.position = "none")
-  
-}
-
-#------------------------------------------------------------------------------
-boxplot_fpkm <- function(goi, cols = group.cols, 
-                    title = blank.title, ylabel = blank.ylabel){
-  
-  blank.title = " "; blank.ylabel = " "
-  group.cols = c("ASO_GF"= "#FF6000", "ASO_SPF" = "#077E97", 
-                 "WT_GF" = "#A00000", "WT_SPF" = "808080")
-  
-  set.seed(123)
-  
-  df.long %>% 
-    filter(Genes == goi) %>% 
-    ggplot(aes(x=group, y= count)) +
-    geom_boxplot(aes(fill = group), alpha = 0.75, outlier.alpha = 0, width = 0.9) +
-    geom_point(aes(fill = group), position = position_jitterdodge(jitter.width = 0.5), 
-               shape=21, size=1.5, alpha = 1) +
-    theme_classic() +
-    ggtitle(goi) +
-    labs(y = "FPKM") +
-    scale_color_manual(values = cols, name ="Group") +
-    scale_fill_manual(values = cols, name ="Group") +
-    theme(plot.title = element_text(hjust = 0.5),
-          axis.title.x = element_blank(),
-          panel.grid.major.y = element_blank(),
-          legend.position = "none")
-  
-}
-
-#------------------------------------------------------------------------------
 '%ni%' <- Negate('%in%')
 
 #------------------------------------------------------------------------------
@@ -160,5 +25,57 @@ color_loop_generator <- function(names_col){
   return(color_output)
 }
 
+
+#--------------------------------------------------------------------------------
+#                  Interaction Effect Plotting Function 
+#--------------------------------------------------------------------------------
+
+interact_plot <- function(df.interact){
+  
+  #' Creates XY plot of Microbiome/Genotype and labels top _25_ significant 
+  #' interaction effect variables
+  
+  df.interact.nonsig <- df.interact %>% filter(`pr_t.MicrobiotaSPF:GenotypeWT` > 0.05)
+  df.interact.sig <- df.interact %>% filter(`pr_t.MicrobiotaSPF:GenotypeWT` <= 0.05) %>% 
+    dplyr::arrange(`pr_t.MicrobiotaSPF:GenotypeWT`, desc(abs(`estimate.MicrobiotaSPF:GenotypeWT`))) %>%
+    slice_head(n = 25)
+  
+  interact.plot <- ggplot() +
+    geom_point(data = df.interact.nonsig, 
+               aes(x = estimate.MicrobiotaSPF, y = estimate.GenotypeWT), 
+               alpha = 0.7, color = "grey") +
+    geom_point(data = df.interact.sig,
+               aes(x = estimate.MicrobiotaSPF, y = estimate.GenotypeWT, 
+                   color = "red")) +
+    theme_bw() +
+    geom_abline(intercept = 0, slope = 1, linetype = 3, color = "darkgrey") +
+    geom_abline(intercept = 0, slope = -1, linetype = 3, color = "darkgrey") +
+    geom_vline(xintercept = 0, linetype = 1, color = "grey") +
+    geom_hline(yintercept = 0, linetype = 1, color = "grey") +
+    ggplot2::annotate("text", x=2.25, y=2.25, label= "SPF WT \n Enriched") + 
+    ggplot2::annotate("text", x=2.25, y=-2.25, label= "SPF ASO \n Enriched") + 
+    ggplot2::annotate("text", x=-2.25, y=2.25, label= "GF WT \n Enriched") + 
+    ggplot2::annotate("text", x=-2.25, y=-2.25, label= "GF ASO \n Enriched") + 
+    lims(x = c(-2.5, 2.5), y = c(-2.5, 2.5)) +
+    labs(x = " Microbiota Effect Estimate (SPF / GF)",
+         y = " Genotype Effect Estimate (WT / ASO)",
+         title = df.interact.sig$tissue) +
+    geom_label_repel(data = df.interact.sig,
+                     aes(x = estimate.MicrobiotaSPF, y = estimate.GenotypeWT, label = metabolite), 
+                     segment.alpha = 0.5,
+                     segment.size = 0.2, 
+                     size = 2.75,
+                     force = 1,
+                     max.time = 1,
+                     max.iter = Inf,
+                     max.overlaps = Inf) +
+    theme(panel.grid = element_blank(),
+          plot.title = element_text(hjust = 0.5),
+          legend.position = "None")
+  interact.plot + 
+    return(interact.plot)
+}
+
+#--------------------------------------------------------------------------------
 
 
--- src/interaction_effect_plots.R
+++ src/interaction_effect_plots.R
@@ -2,59 +2,6 @@
 source("src/load_packages.R")
 
 
-#--------------------------------------------------------------------------------
-#                           Plotting Function 
-#--------------------------------------------------------------------------------
-
-interact_plot <- function(df.interact){
-  
-  #' Creates XY plot of Microbiome/Genotype and labels top _25_ significant 
-  #' interaction effect variables
-  
-  df.interact.nonsig <- df.interact %>% filter(`pr_t.MicrobiotaSPF:GenotypeWT` > 0.05)
-  df.interact.sig <- df.interact %>% filter(`pr_t.MicrobiotaSPF:GenotypeWT` <= 0.05) %>% 
-    dplyr::arrange(`pr_t.MicrobiotaSPF:GenotypeWT`, desc(abs(`estimate.MicrobiotaSPF:GenotypeWT`))) %>%
-    slice_head(n = 25)
-  
-  interact.plot <- ggplot() +
-    geom_point(data = df.interact.nonsig, 
-               aes(x = estimate.MicrobiotaSPF, y = estimate.GenotypeWT), 
-               alpha = 0.7, color = "grey") +
-    geom_point(data = df.interact.sig,
-               aes(x = estimate.MicrobiotaSPF, y = estimate.GenotypeWT, 
-                   color = "red")) +
-    theme_bw() +
-    geom_abline(intercept = 0, slope = 1, linetype = 3, color = "darkgrey") +
-    geom_abline(intercept = 0, slope = -1, linetype = 3, color = "darkgrey") +
-    geom_vline(xintercept = 0, linetype = 1, color = "grey") +
-    geom_hline(yintercept = 0, linetype = 1, color = "grey") +
-    ggplot2::annotate("text", x=2.25, y=2.25, label= "SPF WT \n Enriched") + 
-    ggplot2::annotate("text", x=2.25, y=-2.25, label= "SPF ASO \n Enriched") + 
-    ggplot2::annotate("text", x=-2.25, y=2.25, label= "GF WT \n Enriched") + 
-    ggplot2::annotate("text", x=-2.25, y=-2.25, label= "GF ASO \n Enriched") + 
-    lims(x = c(-2.5, 2.5), y = c(-2.5, 2.5)) +
-    labs(x = " Microbiota Effect Estimate (SPF / GF)",
-         y = " Genotype Effect Estimate (WT / ASO)",
-         title = df.interact.sig$tissue) +
-    geom_label_repel(data = df.interact.sig,
-                     aes(x = estimate.MicrobiotaSPF, y = estimate.GenotypeWT, label = metabolite), 
-                     segment.alpha = 0.5,
-                     segment.size = 0.2, 
-                     size = 2.75,
-                     force = 1,
-                     max.time = 1,
-                     max.iter = Inf,
-                     max.overlaps = Inf) +
-    theme(panel.grid = element_blank(),
-          plot.title = element_text(hjust = 0.5),
-          legend.position = "None")
-  interact.plot + 
-  return(interact.plot)
-}
-
-#--------------------------------------------------------------------------------
-  
-
 df.interact.A <-
   read_excel("files/caltech.PD mice.011920.xlsx", 
              sheet = "interaction.regcoef") %>% 
